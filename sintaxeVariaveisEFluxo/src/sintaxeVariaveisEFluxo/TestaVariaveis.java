595919d4f4f5f52ff44bd33dd925691a4066189b
Danillo Barcelos <danillofernandesb@gmail.com>
2023-06-07 19:15:32 -0300

    primeiros passos

---
+++ sintaxeVariaveisEFluxo/src/sintaxeVariaveisEFluxo/TestaVariaveis.java
@@ -0,0 +1,18 @@
+package sintaxeVariaveisEFluxo;
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Olá, novo teste");
+
+		int idade;
+		idade = 37;
+
+		System.out.println(idade);
+
+		idade = 30 + 10;
+		System.out.println(idade);
+
+		idade = 5 * 7 + 2;
+		System.out.println("A idade é: " + idade);
+	}
+}
