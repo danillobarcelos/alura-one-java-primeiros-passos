eb375b4d8803712a978368b00612c6562c08736c
Danillo Barcelos <danillofernandesb@gmail.com>
2023-06-21 18:43:25 -0300

    herança

---
+++ 03 java polimorfismo/bytebankHerdado/src/bytebankHerdado/Gerente.java
@@ -0,0 +1,23 @@
+package bytebankHerdado;
+
+public class Gerente extends Funcionario{
+
+	private int senha;
+	
+	public boolean autenticacao(int senha) {
+		if(this.senha == senha) {
+			return true;
+		}
+		return false;
+	}
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+//	public double getBonificacao() {
+//		return this.salario;
+//	}
+	
+	
+}
---
+++ 03 java polimorfismo/bytebankHerdado/src/bytebankHerdado/TesteGerente.java
@@ -0,0 +1,25 @@
+package bytebankHerdado;
+
+public class TesteGerente {
+
+	public static void main(String[] args) {
+		Gerente g1 = new Gerente();
+		
+		g1.setNome("Marco");
+		g1.setCpf("111.222.333-44");
+		g1.setSalario(5000);
+		
+		System.out.println(g1.getNome());
+		System.out.println(g1.getCpf());
+		System.out.println(g1.getSalario());
+		
+		g1.setSenha(12345);
+		boolean autenticacao = g1.autenticacao(12345);
+		
+		System.out.println(autenticacao);
+		
+		
+
+	}
+
+}
