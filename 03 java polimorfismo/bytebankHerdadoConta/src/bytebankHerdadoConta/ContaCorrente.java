744aed07ec35959e1fdd5dc5f2e237ff6f460dec
Danillo Barcelos <danillofernandesb@gmail.com>
2023-06-24 10:18:05 -0300

    alternativas para herança

--- 03 java polimorfismo/bytebankHerdadoConta/src/bytebankHerdadoConta/ContaCorrente.java
+++ 03 java polimorfismo/bytebankHerdadoConta/src/bytebankHerdadoConta/ContaCorrente.java
@@ -1,6 +1,6 @@
 package bytebankHerdadoConta;
 
-public class ContaCorrente extends Conta {
+public class ContaCorrente extends Conta implements Tributavel {
 
 	public ContaCorrente(int agencia, int numero) {
 		super(agencia, numero);
@@ -11,6 +11,16 @@ public class ContaCorrente extends Conta {
 	public boolean saca(double valor) {
 		double valorASacar = valor + 0.20;
 		return super.saca(valorASacar);
+	}
+	
+	@Override
+	public void deposita(double valor) {
+		super.deposita(valor);
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.getSaldo() * 0.01;
 	}
 
 }
